ecda43d800b8a5dfa493128a944fb67b7a999e53
alidete13 <alidete13@smpanthers.org>
2016-05-03 13:22:27 -0700

    Initial commit of single-view project.

---
+++ Gael Events/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gael Events
+//
+//  Created by Ayalew Lidete on 5/3/16.
+//  Copyright © 2016 GaelTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
