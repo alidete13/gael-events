17fe579dcd2da4ae94cf7b9c1ceccc9aa1825725
alidete13 <alidete13@smpanthers.org>
2016-05-04 14:14:46 -0700

    Fixed layout constraints and added EventsTableViewController template.

---
+++ Gael Events/EventsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EventsTableViewController.swift
+//  Gael Events
+//
+//  Created by Ayalew Lidete on 5/4/16.
+//  Copyright © 2016 GaelTech. All rights reserved.
+//
+
+import UIKit
+
+import Firebase
+
+class EventsTableViewController: UITableViewController {
+
+}
